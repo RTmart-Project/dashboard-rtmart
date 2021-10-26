ec5aa358d9ca084195ab19d6991650ffc79a1661
anggitow <anggitov@gmail.com>
2021-10-26 20:34:19 +0700

    Add Version

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
     <!-- To the right -->
     <div class="float-right d-none d-sm-inline">
-
+        <b>Version</b> 1.1.0
     </div>
     <!-- Default to the left -->
     <strong>Copyright &copy; 2021 <a target="_blank" href="https://rt-mart.id">RTMart Grup Indonesia</a>.</strong>
