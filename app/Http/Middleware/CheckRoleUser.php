0674e73663e3a1b1d1ac24c56f8635001672955b
anggitow <anggitov@gmail.com>
2022-06-14 09:34:04 +0700

    Add User Activity Log

--- app/Http/Middleware/CheckRoleUser.php
+++ app/Http/Middleware/CheckRoleUser.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class CheckRoleUser
 {
@@ -19,6 +20,18 @@ class CheckRoleUser
     {
         $user = Auth::user();
         if (in_array($user->RoleID, $roles)) {
+            $data = [
+                'UserID' => Auth::user()->UserID,
+                'URL' => $request->path(),
+                'RouteName' => $request->route()->getName(),
+                'IPAddress' => $request->ip(),
+                'Browser' => $request->header('user-agent'),
+                'CreatedDate' => date('Y-m-d H:i:s')
+            ];
+
+            DB::table('ms_user_activity_log')
+                ->insert($data);
+
             return $next($request);
         }
 
