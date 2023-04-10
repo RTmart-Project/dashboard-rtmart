124691b0d952eed38784f04d8b3ede684a2a3d4d
Kito <surya.aw26@gmail.com>
2023-04-10 16:57:33 +0700

    - Change disclaimer format - Create helper convert month to roman numerals

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -49,9 +49,29 @@ class Helper
             return $temp;
         }
 
-        $hasil = trim(penyebut($number)). ' rupiah';
+        $hasil = trim(penyebut($number)) . ' rupiah';
         $result = ucwords($hasil);
 
         return $result;
+    }
+
+    public static function convertMonthToRomanNumerals($monthNumber)
+    {
+        $romanNumerals = array(
+            'I' => 1,
+            'II' => 2,
+            'III' => 3,
+            'IV' => 4,
+            'V' => 5,
+            'VI' => 6,
+            'VII' => 7,
+            'VIII' => 8,
+            'IX' => 9,
+            'X' => 10,
+            'XI' => 11,
+            'XII' => 12,
+        );
+
+        return array_search($monthNumber, $romanNumerals);
     }
 }
